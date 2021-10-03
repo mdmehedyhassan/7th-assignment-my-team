34849e544c493e50877be684af068964d20f9858
unknown <mdmehedyh55@gamil.com>
2021-10-03 14:07:31 +0600

    bootstrap, font awesome

---
+++ src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const Main = () => {
+    return (
+        <div>
+            <h1>Main</h1>
+            <Header></Header>
+        </div>
+    );
+};
+
+export default Main;
